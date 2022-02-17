fc624057e159aae2807b8b671d7ac4a99b09c423
Netacci <chinetaadinnu@gmail.com>
2022-02-17 11:07:13 +0100

    login added

--- part4/app.js
+++ part4/app.js
@@ -6,9 +6,11 @@ const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const blogRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 app.use(express.json());
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
